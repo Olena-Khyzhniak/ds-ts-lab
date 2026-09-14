a04ce44c06fab0a5e1dbd62c516af4b4d2bd774c
Olena-Khyzhniak <20106012@mail.wit.ie>
2026-09-14 11:39:12 +0100

    Refactor - Add type declaration file; disable reduntant console logs.

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -1,28 +1,6 @@
 // (Optional) Change the names below to your friends.
 
-
-
-interface Friend {
-    name: string;
-    phone: string;
-    age: number
-}
-
-interface Colleague {
-    name: string;
-    department: string;
-    contact: {
-        email: string;
-        extension: number;
-    }
-}
-
-
-interface ColleagueHistory {
-  current: Colleague[],
-  former: Colleague[]
-}
-
+import {Friend, Colleague, ColleagueHistory } from './myTypes'
 
 
 
@@ -40,8 +18,11 @@ const friend2: Friend = {
   age: 31,
 };
 
-const friends: Friend[] = [friend1, friend2];
-console.log(friends[1]);
+
+export const friends: Friend[] = [friend1, friend2];
+//console.log(friends[1]);
+
+
 
 //   -------------------
 
@@ -83,4 +64,4 @@ export const colleagues : ColleagueHistory = {
 
 
 
-console.log(colleagues.current[0]);
+//console.log(colleagues.current[0]);
