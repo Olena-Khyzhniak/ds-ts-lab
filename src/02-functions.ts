3e9b96bd2a116d29b534478d201beb92ab99a6dd
Olena-Khyzhniak <20106012@mail.wit.ie>
2026-09-14 12:32:32 +0100

    Higher-order functions

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,6 +1,7 @@
 import {Friend, Colleague } from './myTypes'
 import { friends } from './01-basics';
 import { colleagues } from './01-basics';
+import { EmailContact } from './myTypes';
 
 
 function older(f: Friend) : string {
@@ -49,3 +50,26 @@ function addColleague(
 
 addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
 console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
+
+
+
+function sortColleagues(
+  colleagues: Colleague[],
+  sorter: (c1: Colleague, c2: Colleague) => number
+): EmailContact[] {
+  const sorted = colleagues.sort(sorter); // Colleague[] inferred
+  const result: EmailContact[] = sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
+  return result 
+}
+
+console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
+console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length));
+
+
+function findFriends(friends: Friend[], predicate: (f: Friend) => boolean): string[] {
+  return friends.filter(predicate).map((f) => f.name);
+}
+
+
+console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
+console.log(findFriends(friends, (friend) => friend.age < 35));
